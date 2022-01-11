a47e0f7d82d85de7e43a2a49163dc46a77ba9408
MohammadReza <47322990+immmdreza@users.noreply.github.com>
2022-01-11 20:35:37 +0330

    Added Extensions

---
+++ SimpleUpdateHandler/DependencyInjection/DiExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Telegram.Bot;
+
+namespace SimpleUpdateHandler.DependencyInjection
+{
+    public static class DiExtensions
+    {
+        /// <summary>
+        /// Use this to add <see cref="SimpleDiUpdateProcessor"/> to service collection
+        /// </summary>
+        /// <param name="serviceDescriptors"></param>
+        /// <param name="handlerContainers"></param>
+        public static void AddUpdateProcessor(
+            this IServiceCollection serviceDescriptors,
+            ITelegramBotClient? telegramBotClient = default,
+            params IHandlerContainer[]? handlerContainers)
+        {
+            if (handlerContainers != null)
+            {
+                foreach (var handlerContainer in handlerContainers)
+                {
+                    serviceDescriptors.AddScoped(handlerContainer.HandlerType);
+                }
+            }
+
+            serviceDescriptors.AddSingleton(
+                x => new SimpleDiUpdateProcessor(
+                    telegramBotClient?? x.GetRequiredService<ITelegramBotClient>(),
+                    x.GetRequiredService<IServiceProvider>(),
+                    handlerContainers));
+        }
+    }
+}
