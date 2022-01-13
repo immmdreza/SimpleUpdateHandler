b800ea291d157a43108372a26366946b35003aeb
MohammadReza <47322990+immmdreza@users.noreply.github.com>
2022-01-13 13:29:33 +0330

    Extension fixes

--- SimpleUpdateHandler/DependencyInjection/DiExtensions.cs
+++ SimpleUpdateHandler/DependencyInjection/DiExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Telegram.Bot;
-using Telegram.Bot.Types;
 
 namespace SimpleUpdateHandler.DependencyInjection
 {
@@ -11,51 +10,19 @@ namespace SimpleUpdateHandler.DependencyInjection
         /// </summary>
         public static void AddUpdateProcessor(
             this IServiceCollection serviceDescriptors,
-            ITelegramBotClient? telegramBotClient = default,
-            params IHandlerContainer[]? handlerContainers)
+            Action<UpdateProcessorContext> configure,
+            ITelegramBotClient? telegramBotClient = default)
         {
-            if (handlerContainers != null)
+            var upc = new UpdateProcessorContext(telegramBotClient);
+
+            configure(upc);
+
+            foreach (var handler in upc.HandlerContainers)
             {
-                foreach (var handlerContainer in handlerContainers)
-                {
-                    serviceDescriptors.AddScoped(handlerContainer.HandlerType);
-                }
+                serviceDescriptors.AddScoped(handler.HandlerType);
             }
 
-            var serviceProvoder = 
-            serviceDescriptors.AddSingleton(
-                x => new SimpleDiUpdateProcessor(
-                    telegramBotClient?? x.GetRequiredService<ITelegramBotClient>(),
-                    serviceDescriptors, x,
-                    handlerContainers));
-        }
-
-        /// <summary>
-        /// Use this to add <see cref="SimpleDiUpdateProcessor"/> to service collection
-        /// </summary>
-        public static SimpleDiUpdateProcessor AddUpdateProcessor(
-            this IServiceProvider serviceProvider,
-            IServiceCollection serviceDescriptors,
-            ITelegramBotClient? telegramBotClient = default,
-            params IHandlerContainer[]? handlerContainers)
-        {
-            if (handlerContainers != null)
-            {
-                foreach (var handlerContainer in handlerContainers)
-                {
-                    serviceDescriptors.AddScoped(handlerContainer.HandlerType);
-                }
-            }
-
-            var processor = new SimpleDiUpdateProcessor(
-                    telegramBotClient ??
-                        serviceProvider.GetRequiredService<ITelegramBotClient>(),
-                    serviceDescriptors,
-                    serviceProvider,
-                    handlerContainers);
-
-            serviceDescriptors.AddSingleton(processor);
-            return processor;
+            serviceDescriptors.AddSingleton(x => upc.Build(x));
         }
     }
 }
